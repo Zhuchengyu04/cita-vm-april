09f415ead2b8a06297c06e9f8c1f8c28507c4380
Zhuchengyu04 <zcylce02@gmail.com>
2021-04-02 14:59:06 +0800

    add vc cargo

---
+++ target/debug/build/proc-macro2-1b8c14f4bef23d60/build_script_build-1b8c14f4bef23d60.d
@@ -0,0 +1,5 @@
+c:\Users\zcy\Desktop\论文\cita-vm\target\debug\build\proc-macro2-1b8c14f4bef23d60\build_script_build-1b8c14f4bef23d60.exe: C:\Users\zcy\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.26\build.rs
+
+c:\Users\zcy\Desktop\论文\cita-vm\target\debug\build\proc-macro2-1b8c14f4bef23d60\build_script_build-1b8c14f4bef23d60.d: C:\Users\zcy\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.26\build.rs
+
+C:\Users\zcy\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.26\build.rs:
---
+++ target/debug/build/syn-ca0bd0eb179a28ba/build_script_build-ca0bd0eb179a28ba.d
@@ -0,0 +1,5 @@
+c:\Users\zcy\Desktop\论文\cita-vm\target\debug\build\syn-ca0bd0eb179a28ba\build_script_build-ca0bd0eb179a28ba.exe: C:\Users\zcy\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.68\build.rs
+
+c:\Users\zcy\Desktop\论文\cita-vm\target\debug\build\syn-ca0bd0eb179a28ba\build_script_build-ca0bd0eb179a28ba.d: C:\Users\zcy\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.68\build.rs
+
+C:\Users\zcy\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.68\build.rs:
